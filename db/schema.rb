be9759ccdad337fb3ada8df9ef3c9403dfc85b38
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-07-08 02:04:47 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_27_021953) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "song_id"
     t.string "content"
